ce6d8320ce7717bd2da7e468d88140d2534c8894
Jae-Won Chung <jwnchung@umich.edu>
2023-06-22 16:24:34 -0400

    Make outfile directory

--- scripts/compute_metrics.py
+++ scripts/compute_metrics.py
@@ -10,6 +10,7 @@ def main(data_dir: str, out_file: str) -> None:
     model_names = os.listdir(data_dir)
     print(f"{model_names=}")
 
+    os.makedirs(os.path.dirname(out_file), exist_ok=True)
     out_csv = csv.writer(open(out_file, "w", newline=""))
     metrics = ["throughput", "response_length", "latency", "energy"]
     out_csv.writerow(["model"] + metrics)

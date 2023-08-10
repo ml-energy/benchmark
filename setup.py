d25986acfb4ad506403c47f9efc5f38fa3e58eeb
Jae-Won Chung <jwnchung@umich.edu>
2023-08-10 19:53:46 -0400

    Try to fix HF space build

--- setup.py
+++ setup.py
@@ -6,7 +6,11 @@ extras_require = {
         "fschat==0.2.23",
         "text_generation @ git+https://github.com/ml-energy/text_generation_energy@master",
     ],
-    "app": ["plotly==5.15.0", "gradio==3.39.0", "pydantic==1.10.9"],
+    "app": [
+        "plotly==5.15.0",
+        "gradio==3.39.0",
+        "text_generation @ git+https://github.com/ml-energy/text_generation_energy@master",
+    ],
     "benchmark": ["zeus-ml", "fschat==0.2.23", "tyro", "rich"],
 }
 
